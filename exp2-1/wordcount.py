1c474e70120f263bcfd880c476775dd5a65a065e
Wei-Tsung Su <ellington.su@gmail.com>
2019-01-14 18:27:05 +0800

    revise exp2-1

--- exp2-1/wordcount.py
+++ exp2-1/wordcount.py
@@ -1,18 +1,23 @@
 from pyspark import SparkContext
 
 sc = SparkContext()
-# Step 1. Read text file to RDD
-text_file = sc.textFile("textfile")
-#print text_file.collect()
+# Step 1.
+# Read text file to RDD
+tf = sc.textFile("textfile")
+#print tf.collect()
 
-# Step 2. Read and split each line by " "
-counts = text_file.flatMap(lambda line: line.split(" "))
-#print counts.collect()
+# Step 2.
+# Read and split each line by " "
+wc = tf.flatMap(lambda line: line.split(" "))
+#print wc.collect()
 
-# Step 3. Read and output key-value pair (word, 1) for each word
-counts = counts.map(lambda word: (word, 1))
-#print counts.collect()
+# Step 3. 
+# Read and output key-value pair (word, 1)
+wc = wc.map(lambda word: (word, 1))
+#print wc.collect()
 
-# Step 4. If the keys of any two key-value pairs are the same, sum up their values
-counts = counts.reduceByKey(lambda a, x: a+x)
-print counts.collect()
+# Step 4. 
+# Sum up the values of the (word, 1) pairs 
+# which have the same key  
+wc = wc.reduceByKey(lambda a, x: a+x)
+print wc.collect()
